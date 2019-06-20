f4482be211148dbe644bd32f86d7a87404c0f81e
Hleb Kanonik <50049166+Cyberglamdring@users.noreply.github.com>
2019-06-20 19:30:36 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -2,7 +2,7 @@
 def paintd():
     N, M = map(int, input('Input values separated by a space (e.g.: 11 33): ').split())
     a = [('.|.' * i).center(M, '-') for i in range(1, N, 2)]
-    for e in a + ['WELCOME'.center(M, '-')] + list(reversed(a)): 
+    for e in a + ['WELCOME'.center(M, '-')] + list(reversed(a)):
         print(e)
     return e
 
