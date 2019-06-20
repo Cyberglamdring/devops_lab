6135b47d366617478d89e245429093e8c949f0ad
Hleb Kanonik <50049166+Cyberglamdring@users.noreply.github.com>
2019-06-20 19:24:21 +0300

    edit (flake8)

--- task6.py
+++ task6.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 firstinput = int(input('Input int M = ',))
-setone = set(map(int,input('Input contains M space-separeted int: ',).split()))
+setone = set(map(int, input('Input contains M space-separeted int: ',).split()))
 
 secondinput = int(input('Input int N = ',))
-settwo = set(map(int,input('Input contains N space-separeted int: ',).split()))
+settwo = set(map(int, input('Input contains N space-separeted int: ',).split()))
 
 settree = (setone.difference(settwo)).union(settwo.difference(setone))
 
-print ("Summetric difference:")
-for i in sorted(list(settree)):print (i)
+print("Summetric difference:")
+for i in sorted(list(settree)):
+    print(i)
