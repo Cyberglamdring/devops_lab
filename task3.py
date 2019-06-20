4f1a7332747f00000507ec61a718a44c38b8051f
Hleb Kanonik <50049166+Cyberglamdring@users.noreply.github.com>
2019-06-20 19:24:52 +0300

    edit (flake8)

--- task3.py
+++ task3.py
@@ -3,8 +3,9 @@ def factcount():
     "Calculate factorial"
     x = int(input("Intput the number: "))
     factorial = 1
-    for i in range(2, x+1):
+    for i in range(2, x + 1):
         factorial *= i
     return factorial
 
-print ("N! =",factcount())
+
+print("N! =", factcount())
