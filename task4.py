689417997f0fcb3e3589cb694e9ae35b13389f60
Hleb Kanonik <50049166+Cyberglamdring@users.noreply.github.com>
2019-06-20 19:25:12 +0300

    edit (flake8)

--- task4.py
+++ task4.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 def paintd():
     N, M = map(int,input('Input values separated by a space (e.g.: 11 33): ').split())
-    a = [('.|.'*i).center(M,'-') for i in range(1,N,2)]
-    for e in a+['WELCOME'.center(M,'-')]+list(reversed(a)):print(e)
+    a = [('.|.' * i).center(M, '-') for i in range(1, N, 2)]
+    for e in a + ['WELCOME'.center(M, '-')] + list(reversed(a)): print(e)
     return e
-print (paintd())
+
+
+print(paintd())
