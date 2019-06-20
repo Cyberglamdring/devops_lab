5e6b789f3e42cfcd93b255adb95648040dddb1cd
Hleb Kanonik <50049166+Cyberglamdring@users.noreply.github.com>
2019-06-20 19:04:51 +0300

    update task5

--- task5.py
+++ task5.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
-position_y = 0
-position_x = 0
+move = input("Input step: ",)
 
-if move == UP:
-    position_y += 1
-elif move == D
-    position_y -= 1
-elif move == L
-    position_x += 1
-elif move == R
-    position_x -= 1
+position_x = 0
+position_y = 0
+
+count = len(move)
+
+for i in range(count):
+    if move[i] == "U":
+        position_y += 1
+    elif move[i] == "D":
+        position_y -= 1
+    elif move[i] == "L":
+        position_x += 1
+    elif move[i] == "R":
+        position_x -= 1
+
+if (position_x == 0) and (position_y == 0):
+    print("True")
+else:
+    print("False")
