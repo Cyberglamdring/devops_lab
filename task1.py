e648e1b542b394eb36022b3ea8023b9e525b9f4f
Hleb Kanonik <50049166+Cyberglamdring@users.noreply.github.com>
2019-06-20 19:25:47 +0300

    edit (flake8)

--- task1.py
+++ task1.py
@@ -5,11 +5,13 @@ def markcount():
         student_marks = {}
         for _ in range(n):
             name, *line = input("Input NAME and MARKS separated by a space: ",).split()
-            scores = list (map(float, line))
+            scores = list(map(float, line))
             student_marks[name] = scores
-        query_name = input ("NAME of student: ",)
+        query_name = input("NAME of student: ",)
 
     x = sum(student_marks[query_name])/3
     return x
 
-print (markcount())
+
+
+print(markcount())
